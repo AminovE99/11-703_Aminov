3978adb82e0b34e5976aa4aa1d965149fcbfe074
emil <amionv99@gmail.com>
2020-09-11 15:40:19 +0300

    :construction: Visualisation data by matplotlib library
    Signed-off-by: emil <amionv99@gmail.com>

---
+++ tasks/1_visualization_csv.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+def count_age(test_dataset, survived_dataset):
+    return dict(Counter([round(value) for value in test_dataset.Age if not math.isnan(value)]))
+
+
+
+if __name__ == '__main__':
+    test_dataset = pd.read_csv("/home/emil/Desktop/MachineLearningLessons/titanic_dataset/test.csv")
+    survived_dataset = pd.read_csv("/home/emil/Desktop/MachineLearningLessons/titanic_dataset/gender_submission.csv")
+    age_count = count_age(test_dataset, survived_dataset)
+    plt.xlabel('Age')
+    plt.ylabel('Count')
+    plt.plot(age_count.keys(), age_count.values(), c='blue', marker='x')
+    plt.show()
