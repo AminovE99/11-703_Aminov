53be5af3636622eb42d4945f15b3dc64e14200ec
emil <amionv99@gmail.com>
2020-09-11 16:19:29 +0300

    :sparkles: Graph of Count of people and age
    Signed-off-by: emil <amionv99@gmail.com>

--- tasks/1_visualization_csv.py
+++ tasks/1_visualization_csv.py
@@ -4,16 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
+
 def count_age(test_dataset, survived_dataset):
+    """Return count of passangers. Key is an age of person."""
     return dict(Counter([round(value) for value in test_dataset.Age if not math.isnan(value)]))
 
 
-
-if __name__ == '__main__':
+def show_count_age():
     test_dataset = pd.read_csv("/home/emil/Desktop/MachineLearningLessons/titanic_dataset/test.csv")
     survived_dataset = pd.read_csv("/home/emil/Desktop/MachineLearningLessons/titanic_dataset/gender_submission.csv")
+
     age_count = count_age(test_dataset, survived_dataset)
+
     plt.xlabel('Age')
     plt.ylabel('Count')
-    plt.plot(age_count.keys(), age_count.values(), c='blue', marker='x')
+
+    plt.scatter(age_count.keys(), age_count.values(), c='blue')
     plt.show()
+
+
+if __name__ == '__main__':
+    show_count_age()
